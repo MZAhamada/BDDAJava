e0eca305365c29a95f31ef81e99c3d61bc4e5167
MZAhamada <55712733+MZAhamada@users.noreply.github.com>
2019-10-07 16:03:12 +0200

    Add files via upload

---
+++ Projet_Turam_Ahamada_Tran/Code/src/Frame.java
@@ -0,0 +1,39 @@
+public class Frame {
+
+    private int pinCount = 0;
+    private boolean flagDirty = false;
+    private byte[] buff = new byte[(int)Constants.pageSize];
+    private PageId pageId = null;
+
+    public int getPinCount() {
+        return pinCount;
+    }
+
+    public void setPinCount(int pinCount) {
+        this.pinCount = pinCount;
+    }
+
+    public boolean isFlagDirty() {
+        return flagDirty;
+    }
+
+    public void setFlagDirty(boolean flagDirty) {
+        this.flagDirty = flagDirty;
+    }
+
+    public byte[] getBuff() {
+        return buff;
+    }
+
+    public void setBuff(byte[] buff) {
+        this.buff = buff;
+    }
+
+    public PageId getPageId() {
+        return pageId;
+    }
+
+    public void setPageId(PageId pageId) {
+        this.pageId = pageId;
+    }
+}
