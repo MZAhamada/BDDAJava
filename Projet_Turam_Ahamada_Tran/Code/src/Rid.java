894c84ded15ee0c4dd3f9429b0499983971ee151
MZAhamada <55712733+MZAhamada@users.noreply.github.com>
2019-10-21 14:06:27 +0200

    Add files via upload

---
+++ Projet_Turam_Ahamada_Tran/Code/src/Rid.java
@@ -0,0 +1,27 @@
+package manager;
+
+public class Rid {
+    private PageId pageid;
+    private int slotIdx;
+
+    public Rid(PageId pageid, int slotIdx) {
+        this.pageid = pageid;
+        this.slotIdx = slotIdx;
+    }
+
+    public PageId getPageid() {
+        return pageid;
+    }
+
+    public void setPageid(PageId pageid) {
+        this.pageid = pageid;
+    }
+
+    public int getSlotIdx() {
+        return slotIdx;
+    }
+
+    public void setSlotIdx(int slotIdx) {
+        this.slotIdx = slotIdx;
+    }
+}
