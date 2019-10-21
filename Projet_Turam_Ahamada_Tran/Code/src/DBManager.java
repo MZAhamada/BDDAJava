eaefc142fa08317975d7e926845cf476825d3ba9
IceBirdCiel <55986654+IceBirdCiel@users.noreply.github.com>
2019-10-21 14:32:34 +0200

    Update DBManager.java

--- Projet_Turam_Ahamada_Tran/Code/src/DBManager.java
+++ Projet_Turam_Ahamada_Tran/Code/src/DBManager.java
@@ -1,5 +1,8 @@
 package manager;
 
+import manager.RelDef;
+import java.util.*;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class DBManager {
@@ -8,8 +11,8 @@ public class DBManager {
 	}
 	private static DBManager INSTANCE = new DBManager();
 	
-	public RelDef createRelation (String nomRel, int nbCol, String [] typeCol) {
-		RelDef relation = new RelDef(nomRel,nbCol,typeCol);
+	public RelDef createRelation (String nomRel,int nbCol, String [] typeCol, int fileIdx, int recordSize, int slotCount) {
+		RelDef relation = new RelDef(nomRel,nbCol,typeCol, fileIdx,recordSize, slotCount);
 		return relation;
 	}
 	
@@ -22,7 +25,6 @@ public class DBManager {
 	
 	public void finish() {
 		DBDef.getInstance().finish();
-		BufferManager.getInstance().FlushAll();
 	}
 	
 	public void processCommand(String s) { //ptt return String plus tard
@@ -34,11 +36,11 @@ public class DBManager {
 		else {
 			switch(token){
 			case "create" :
-				// Bien verifié que les arguments se de bons types
+				// Bien verifier que les arguments sont de bons types
 				/*
 				 * if hasmoretoken error ( on peut le mettre dès l'entrée du nb de colonne)
 				 * appeler le constructeur de reldef
-				 * stocker dans des cases differentes les manager.RelDef
+				 * stocker dans des cases differentes les RelDef
 				 */
 				String nomRelation = st.nextToken();
 				// Chaine de caractere de taille fixe TP1 J1
@@ -47,7 +49,10 @@ public class DBManager {
 				for (int i=0;i<nbColonne;i++) {
 					typeCol[i]=st.nextToken();
 				}
-				RelDef create = createRelation (nomRelation,nbColonne,typeCol);
+				int fileIdx = Integer.parseInt(st.nextToken());
+				int recordSize = 0;
+				int slotCount = recordSize/fileIdx;
+				RelDef create = createRelation (nomRelation,nbColonne,typeCol,fileIdx,recordSize,slotCount);
 				break;
 			case "delete" ://
 				break;
@@ -56,17 +61,33 @@ public class DBManager {
 			}
 		}
 		
-	}	
+	}
+
+	public static int recordSize(String[] s){
+        int recordSize = 0;
+        for(int i =0;i<s.length;i++){
+
+            if (s[i].charAt(0) == 'i') {
+                recordSize+=4;
+            }
+            if (s[i].charAt(0) == 'f') {
+                int w = s[i].charAt(s[i].length() - 1);
+                recordSize+=4*w;
+            }
+            if (s[i].charAt(0) == 's') {
+                recordSize+=4;
+            }
+        }
+        return recordSize;
+    }
+    
+    //Calcul du nombre de slot qu'on peut mettre dans un record
+    public static long slotCount(String[] s){
+        return Constants.pageSize/recordSize(s);
+    }
+    
+    
 	public static DBManager getInstance()
     {   return INSTANCE;
     }
-	
-	/*
-	public  String saisie() {
-		Scanner sc = new Scanner (System.in);
-		String s = sc.nextLine();
-		sc.close();
-		return s;	
-	}
-	*/
 }
